73e4d8ae83777b645ff37aceff861572e1f2e8fc
lucas <lucas97lopes@hotmail.com>
2021-11-02 19:29:55 -0300

    methods

--- 1-fundamentos-nodejs/src/index.js
+++ 1-fundamentos-nodejs/src/index.js
@@ -7,4 +7,46 @@ app.get("/", (request, response) => {
   return response.json({ message: "Hello World Ignite!" })
 })
 
+app.get("/courses", (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Cusro 2",
+    "Curso 3"
+  ])
+})
+
+app.post("/courses", (request, response) => {
+  return response.json([
+    "Curso 1",
+    "Cusro 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.put("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Cusro 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Cusro 2",
+    "Curso 3",
+    "Curso 4"
+  ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json([
+    "Curso 6",
+    "Cusro 2",
+    "Curso 4"
+  ])
+})
 app.listen(port, () => console.log(`Initialized on http://localhost:${port}`))
